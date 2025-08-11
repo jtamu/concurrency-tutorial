472ceab7a5e6aa686bfa4e792ce9766c822869fd
jtamu <gtamu.dip@gmail.com>
2025-08-11 15:38:28 +0900

    feat: shared_ipc

---
+++ src/chapter5/shared_ipc.py
@@ -0,0 +1,53 @@
+"""Using chared memory IPC between threads"""
+
+import time
+from threading import Thread, current_thread
+
+SIZE = 5
+# setup shared memory
+shared_memory = [-1] * SIZE
+
+
+class Producer(Thread):
+    def run(self) -> None:
+        self.name = "Producer"
+        global shared_memory
+        for i in range(SIZE):
+            print(f"{current_thread().name}: Writinng {int(i)}")
+            shared_memory[i - 1] = i
+
+
+class Consumer(Thread):
+    def run(self) -> None:
+        self.name = "Consumer"
+        global shared_memory
+        for i in range(SIZE):
+            while True:
+                line = shared_memory[i]
+                if line == -1:
+                    # data hasn't change - waiting for a second
+                    print(
+                        f"{current_thread().name}: Data not available\n"
+                        f"Sleeping for 1 second before retrying"
+                    )
+                    time.sleep(1)
+                    continue
+                print(f"{current_thread().name}: Read: {int(line)}")
+                break
+
+
+def main() -> None:
+    threads = [Producer(), Consumer()]
+
+    # start threads
+    for thread in threads:
+        thread.start()
+        time.sleep(0.1)
+
+    # block the main thread until the chlid threads has finished
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    main()
